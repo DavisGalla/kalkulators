47b2747cee2db82711be4e3c83434937a3c8c336
DavisGalla <98390378+DavisGalla@users.noreply.github.com>
2023-03-28 12:39:17 +0300

    updated 28.03
    updated 28.03

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const input = document.querySelector('.js-teksts');
 const plusBotton = document.querySelector('.js-plus')
 
-console.log(input)
-console.log(plusBotton)
+const Add = (skaitlis1, skaitlis2) => {
+    return skaitlis1 + skaitlis2
+}
+
+const Subtract = (skaitlis1, skaitlis2) => {
+    return skaitlis1 - skaitlis2
+}
+
+const Multiply = (skaitlis1, skaitlis2) => {
+    return skaitlis1 * skaitlis2
+}
+
+const devide = (skaitlis1, skaitlis2) => {
+    return skaitlis1 / skaitlis2
+}
+
+plusButton.addEventListener('click',() =>{
+    console.log("Mani nospied")
+})
