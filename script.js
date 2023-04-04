b50dff4241af929e67559f4372d58a212955e244
DavisGalla <98390378+DavisGalla@users.noreply.github.com>
2023-04-04 12:33:01 +0300

    04.04
    04.04

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const input = document.querySelector('.js-teksts');
-const plusBotton = document.querySelector('.js-plus')
+const input1 = document.querySelector('.js-teksts1');
+const input2 = document.querySelector('.js-teksts2');
+const plusButton = document.querySelector('.js-plus');
+const minusButton = document.querySelector('.js-minus');
+const multiplyButton = document.querySelector('.js-multiply');
+const divideButton = document.querySelector('.js-divide');
+const result = document.querySelector('.js-result');
+
 
 const Add = (skaitlis1, skaitlis2) => {
-    return skaitlis1 + skaitlis2
+    return skaitlis1) + Number(skaitlis2)
 }
 
 const Subtract = (skaitlis1, skaitlis2) => {
@@ -13,10 +19,10 @@ const Multiply = (skaitlis1, skaitlis2) => {
     return skaitlis1 * skaitlis2
 }
 
-const devide = (skaitlis1, skaitlis2) => {
+const Divide = (skaitlis1, skaitlis2) => {
     return skaitlis1 / skaitlis2
 }
 
-plusButton.addEventListener('click',() =>{
-    console.log("Mani nospied")
+plusButton.addEventListener('click', () => {
+   result.innerHTML = Add(Number(input1.value), Number(input2.value))
 })
